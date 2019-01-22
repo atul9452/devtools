68f139f16130940a4cb8471299d098b172c32413
Atul6 Singh <Atul6.Singh@ril.com>
2019-01-22 10:59:59 +0530

    With DB Server

---
+++ pdo/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Configuration for database connection
+ *
+ */
+
+$host       = "10.157.194.91";
+$username   = "root";
+$password   = "123";
+$dbname     = "test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
